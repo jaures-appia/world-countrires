cd7db948ce2c1a64bbc813d9fce780d0ccb8374f
jaures-appia <appia.ngouan@uvci.edu.ci>
2021-05-19 16:28:30 +0200

    add tab panels

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,88 +1,149 @@
 <template>
   <q-page v-if="countries" class="q-pa-xs bgImage">
-      <div class="row flex flex-center q-pa-lg q-mb-md">
-        <div class="col-lg-4 col-md-5 col-sm-8 col-xs-12">
-          <div class="q-my-md text-bold text-center" style="font-size: 20px">Ready to learn about the countries of the world</div>
-          <q-select
-            outlined
-            v-model="country"
-            clearable
-            use-input
-            input-debounce="0"
-            label="search country"
-            :options="options"
-            @filter="filterFn"
-            color="dark"
-          >
-            <template v-slot:no-option>
-              <q-item>
-                <q-item-section class="text-grey">
-                  No results
-                </q-item-section>
-              </q-item>
-            </template>
-          </q-select>
-        </div>
-      </div>
+    <q-card class="transparent">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-dark"
+        active-color="primary"
+        indicator-color="primary"
+        narrow-indicator
+      >
+        <q-tab name="accueil" label="Accueil" />
+        <q-tab name="stats" label="Statistique" />
+      </q-tabs>
 
-      <div v-if="singleCountry.name" class="row justify-center" style="height: 65%">
-        <q-card class="my-card q-ma-sm col-lg-3 col-md-4 col-sm-6 col-xs-10" style="height: 90%">
-          <q-img style="height: 45%" :src="singleCountry.flag" />
+      <q-separator />
 
-          <q-card-section>
-            <div class="row no-wrap items-center">
-              <div class="col text-h6 ellipsis">
-                {{ singleCountry.name }} ({{ singleCountry.alpha3Code }})
-              </div>
+      <q-tab-panels v-model="tab" animated class="transparent">
+        <q-tab-panel name="accueil">
+          <div class="row flex flex-center q-pa-md q-mb-md">
+            <div class="col-lg-4 col-md-5 col-sm-8 col-xs-12">
+              <div class="q-my-md text-bold text-center" style="font-size: 20px">Ready to learn about the countries of the world ?</div>
+              <q-select
+                outlined
+                v-model="country"
+                clearable
+                use-input
+                input-debounce="0"
+                label="search country"
+                :options="options"
+                @filter="filterFn"
+                color="dark"
+              >
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
             </div>
-          </q-card-section>
+          </div>
 
-          <q-card-section class="q-pt-none">
-            <div class="text-caption">
-              capital, {{ singleCountry.capital }}, {{ singleCountry.name }} is an {{ singleCountry.region }} 
-              country with {{ formatNum(singleCountry.population) }} inhabitants over {{ formatNum(singleCountry.area) }} km2
-            </div>
-            <div class="text-caption q-mt-xs">
-              <span class="text-bold">languages:</span>  
-              <span v-for="(language, i) in singleCountry.languages" :key="i">
-                {{ language.name }} <span v-if="i != singleCountry.languages.length -1">,</span> 
-              </span>
-            </div>
-          </q-card-section>
+          <div v-if="singleCountry.name" class="row justify-center" style="height: 65%">
+            <q-card class="my-card q-ma-sm col-lg-3 col-md-4 col-sm-6 col-xs-10" style="height: 90%">
+              <q-img style="height: 45%" :src="singleCountry.flag" />
 
-          <q-separator />
+              <q-card-section>
+                <div class="row no-wrap items-center">
+                  <div class="col text-h6 ellipsis">
+                    {{ singleCountry.name }} ({{ singleCountry.alpha3Code }})
+                  </div>
+                </div>
+              </q-card-section>
 
-          <q-card-section>
-            <q-btn flat size="sm" :label="'+' + singleCountry.callingCodes[0]">
-              <q-tooltip>
-                callingCodes
-              </q-tooltip>
-            </q-btn> 
-            <q-btn flat size="sm" :label="singleCountry.currencies[0].name">
-              <q-tooltip>
-                currency
-              </q-tooltip>
-            </q-btn>
+              <q-card-section class="q-pt-none">
+                <div class="text-caption">
+                  capital, {{ singleCountry.capital }}, {{ singleCountry.name }} is an {{ singleCountry.region }} 
+                  country with {{ formatNum(singleCountry.population) }} inhabitants over {{ formatNum(singleCountry.area) }} km2
+                </div>
+                <div class="text-caption q-mt-xs">
+                  <span class="text-bold">languages:</span>  
+                  <span v-for="(language, i) in singleCountry.languages" :key="i">
+                    {{ language.name }} <span v-if="i != singleCountry.languages.length -1">,</span> 
+                  </span>
+                </div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-section>
+                <q-btn flat size="sm" :label="'+' + singleCountry.callingCodes[0]">
+                  <q-tooltip>
+                    callingCodes
+                  </q-tooltip>
+                </q-btn> 
+                <q-btn flat size="sm" :label="singleCountry.currencies[0].name">
+                  <q-tooltip>
+                    currency
+                  </q-tooltip>
+                </q-btn>
+                
+              </q-card-section>
+
+            </q-card>
+
+          </div>
+          
+          <div v-else class="row q-gutter-md justify-center" style="height: 65%">
+            <q-carousel
+              v-if="cardWidth.name == 'sm' || cardWidth.name == 'xs'"
+              animated
+              v-model="slide"
+              control-color="primary"
+              navigation
+              infinite
+              class="col-sm-8 col-xs-10 transparent q-pb-xl"
+            >
+              <q-carousel-slide v-for="(c, index) in homeCard" :key="index" :name="index">
+                <q-card class="my-card col-lg-3 col-md-3 col-sm-4 col-xs-10 ">
+                  <q-img style="height: 45%" :src="countries[c].flag" />
+
+                  <q-card-section style="height: 55%">
+                    <q-card-section class="q-pa-none">
+                        <div class="text-bold" style="font-size: 16px">
+                          {{ countries[c].name }} ({{ countries[c].alpha3Code }})
+                        </div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pa-xs">
+                      <div class="text-caption">
+                        capital, {{ countries[c].capital }}, {{ countries[c].name }} is an {{ countries[c].region }} 
+                        country with {{ formatNum(countries[c].population) }} inhabitants over {{ formatNum(countries[c].area) }} km2
+                      </div>
+                      <div class="text-caption q-mt-xs">
+                        <span class="text-bold">languages:</span> 
+                        <span v-for="(language, i) in countries[c].languages" :key="i">
+                          {{ language.name }} <span v-if="i != countries[c].languages.length -1">,</span> 
+                        </span>
+                      </div>
+                    </q-card-section>
+
+                    <q-separator />
+
+                    <q-card-section>
+                      <q-btn flat size="sm" :label="'+' + countries[c].callingCodes[0]">
+                        <q-tooltip>
+                          callingCodes
+                        </q-tooltip>
+                      </q-btn> 
+                      <q-btn flat size="sm" :label="countries[c].currencies[0].name">
+                        <q-tooltip>
+                          currency
+                        </q-tooltip>
+                      </q-btn>
+                    </q-card-section>
+                  </q-card-section>
+
+                </q-card>
+              </q-carousel-slide>
+            </q-carousel>
+
             
-          </q-card-section>
-
-        </q-card>
-
-      </div>
-      
-      <div v-else class="row q-gutter-md justify-center" style="height: 65%">
-        <q-carousel
-          v-if="cardWidth.name == 'sm' || cardWidth.name == 'xs'"
-          animated
-          v-model="slide"
-          control-color="primary"
-          navigation
-          infinite
-          class="col-sm-8 col-xs-10 transparent q-pb-xl"
-        >
-          <q-carousel-slide v-for="(c, index) in homeCard" :key="index" :name="index">
-            <q-card class="my-card col-lg-3 col-md-3 col-sm-4 col-xs-10 ">
-              <q-img style="height: 45%" :src="countries[c].flag" />
+            <q-card v-else class="my-card col-lg-3 col-md-3 col-sm-4 col-xs-10 " style="height: 90%" v-for="(c, index) in homeCard" :key="index">
+              <q-img style="height: 250px; width: 100%" :src="countries[c].flag"  spinner-color="white" />
 
               <q-card-section style="height: 55%">
                 <q-card-section class="q-pa-none">
@@ -121,55 +182,20 @@
               </q-card-section>
 
             </q-card>
-          </q-carousel-slide>
-        </q-carousel>
 
-        
-        <q-card v-else class="my-card col-lg-3 col-md-3 col-sm-4 col-xs-10 " style="height: 90%" v-for="(c, index) in homeCard" :key="index">
-          <q-img style="height: 250px; width: 100%" :src="countries[c].flag"  spinner-color="white" />
+          </div>
 
-          <q-card-section style="height: 55%">
-            <q-card-section class="q-pa-none">
-                <div class="text-bold" style="font-size: 16px">
-                  {{ countries[c].name }} ({{ countries[c].alpha3Code }})
-                </div>
-            </q-card-section>
+          <q-page-sticky position="bottom-right" :offset="[10, 10]">
+            <div class="text-caption">by Jaures Appia &#128526;</div>
+          </q-page-sticky>
+        </q-tab-panel>
 
-            <q-card-section class="q-pa-xs">
-              <div class="text-caption">
-                capital, {{ countries[c].capital }}, {{ countries[c].name }} is an {{ countries[c].region }} 
-                country with {{ formatNum(countries[c].population) }} inhabitants over {{ formatNum(countries[c].area) }} km2
-              </div>
-              <div class="text-caption q-mt-xs">
-                <span class="text-bold">languages:</span> 
-                <span v-for="(language, i) in countries[c].languages" :key="i">
-                  {{ language.name }} <span v-if="i != countries[c].languages.length -1">,</span> 
-                </span>
-              </div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-section>
-              <q-btn flat size="sm" :label="'+' + countries[c].callingCodes[0]">
-                <q-tooltip>
-                  callingCodes
-                </q-tooltip>
-              </q-btn> 
-              <q-btn flat size="sm" :label="countries[c].currencies[0].name">
-                <q-tooltip>
-                  currency
-                </q-tooltip>
-              </q-btn>
-            </q-card-section>
-          </q-card-section>
-
-        </q-card>
-
-      </div>
-      <q-page-sticky position="bottom-right" :offset="[10, 10]">
-        <div class="text-caption">by Jaures Appia &#128526;</div>
-      </q-page-sticky>
+        <q-tab-panel name="stats">
+          <div class="text-h6">Stats</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
 
   </q-page>
 </template>
@@ -181,18 +207,20 @@ export default {
   name: 'PageIndex',
   data(){
     return{
+      tab: 'accueil',
       slide: 1,
       country: "",
-      countries: null,
+      countries: [],
       url: "https://restcountries.eu/rest/v2/all",
-      test: ["a", "b", "c"],
       options: this.myCountries,
-      num: 100000000
     }
   },
   computed: {
     myCountries(){
-      return this.countries.map(c => c.name)
+      return this.countries.map(c => "(+" + c.callingCodes[0] + ") " + c.name )
+    },
+    myCallingCodes(){
+      return this.countries.map(c => c.callingCodes[0])
     },
     homeCard(){
       let a = []
@@ -204,8 +232,10 @@ export default {
     singleCountry(){
       let mySingleCountry = []
       for (let i = 0; i < this.countries.length; i++) {
-        if (this.countries[i].name == this.country) {
-          mySingleCountry = this.countries[i]
+        if (this.country != null) {
+          if (this.country.includes(this.countries[i].name)) {
+            mySingleCountry = this.countries[i]
+          }
         } 
       }
       return mySingleCountry
@@ -228,7 +258,7 @@ export default {
         update(() => {
           this.options = this.myCountries
 
-          // with Quasar v1.7.4+
+          // with Quasar v1.7.4+ .concat(this.myCallingCodes)
           // here you have access to "ref" which
           // is the Vue reference of the QSelect
         })
